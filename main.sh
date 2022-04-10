212ed0f3700101d4f7892c718488c2327290fda7
sbwml <984419930@qq.com>
2022-04-10 23:19:49 +0800

    init

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -d "~/nginx" ];then
+	\cp -ax .nginx ~/nginx
+fi
+
+v2ray/sbin/v2ray -config v2ray/etc/config.json >/dev/null 2>&1 &
+/home/runner/nginx/sbin/nginx -g 'daemon off;'
